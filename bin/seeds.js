ea4eded3b5253f67de02fe8c3daf51e3f48d9ef0
Ignacio Romaní <ignacioromani@gmail.com>
2022-08-29 18:32:36 +0200

    gift seeds created

---
+++ bin/seeds.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Gift = require('../models/Gift.model');
+const GIFTS = require('../data/gifts.json')
+// Conectarme a la base de datos
+
+require('../config/db.config');
+
+mongoose.connection.once('open', () => {
+  mongoose.connection.db.dropDatabase()
+    .then(() => {
+      console.info('Db dropped')
+
+      return Gift.create(GIFTS)
+    })
+    .then(createdGifts => {
+      createdGifts.forEach(gift => console.log(`${gift.name} was created`))
+
+      // Cerrar la conexion
+      return mongoose.connection.close()
+    })
+    .then(() => {
+      console.log('Connection closed')
+
+      process.exit(1)
+    })
+    .catch(err => {
+      console.error(err)
+      process.exit(0)
+    })
+})
