f0dd1dc6fec7cd181209ab1a9e132f13b7b96620
Ignacio Romaní <ignacioromani@gmail.com>
2022-09-10 12:07:29 +0200

    ticket creation and update done

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -4,23 +4,41 @@ const Wedding = require("../models/Wedding.model")
 const User = require("../models/User.model")
 
 module.exports.create = (req, res, next) => {
-    res.render("ticket/form")
+    const { id } = req.params
+    res.render("ticket/form", {weddingId: id})
 }
 
 module.exports.doCreate = (req, res, next) => {
     const { id } = req.params
     const user = req.user
+    req.body.user = user.id
     req.body.wedding = id
+    req.body.bus = Boolean(req.body.bus)
+    req.body.hotel = Boolean(req.body.hotel)
+    console.log(req.body);
 
-    Ticket.create(req.body)
-      .then((ticket) => {
-        console.log(user.id);
-        ticket.user = user.id
-        ticket.wedding = id
-        ticket.save()
-        res.redirect(`/profile`, { id })
+    Ticket.findOne({ wedding: id, user: user.id })
+      .then(ticket => {
+        if (!ticket) {
+          Ticket.create(req.body)
+            .then((ticket) => {
+            //console.log(ticket);
+            res.redirect(`/profile`)
+          })
+          .catch((err) => {
+            //console.error(req.body, err);
+            res.renderWithErrors("ticket/form")
+          })
+        } else {
+          Ticket.findOneAndUpdate(ticket, req.body, { new: true })
+            .then(ticket => {
+              //console.log(req.body);
+              res.redirect('/profile')
+            })
+            .catch(err => {
+              console.error(req.body, err)
+            })
+        }
       })
-      .catch((err) => {
-        res.renderWithErrors("ticket/form")
-      })
+      
 }
