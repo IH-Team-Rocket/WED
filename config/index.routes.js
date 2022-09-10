e3d5b6fd661affceb7456a8f9cc90cdd643b5307
Eduard Tudorache <edy_edytza2003@yahoo.com>
2022-09-10 12:45:18 +0200

    dev things

--- config/index.routes.js
+++ config/index.routes.js
@@ -46,5 +46,6 @@ router.post("/wedding/:id/createGift", giftsController.doCreate)
 //TICKET
 router.get("/wedding/:id/ticket", authMiddlewares.isAuthenticated, ticketController.create)
 router.post("/wedding/:id/ticket", ticketController.doCreate)
+router.get("/wedding/:id/ticket/list", ticketController.list)
 
 module.exports = router
